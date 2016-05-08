76702ab8bc153a5d2f534fd5b69d886fd68985df
dcyoung <dcyoung@go.wustl.edu>
2016-05-07 23:31:19 -0500

    Added a manager for events related to forming new connections. NOT YET implemented new connections.

---
+++ FlowPath/Assets/InteractiveObjects/Mode Selection/Scripts/ProspectiveConnectionManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum ProspectiveConnectionState
+{
+    Latent, //No nodes specified
+    Initiated,   //Start node has been specified
+    Completed    //Start AND End nodes HAVE been specified
+}
+
+public static class ProspectiveConnectionManager{
+
+    //The current connection to be formed. Will require a user to specify a start and end node.
+    public static ProspectiveConnectionState currentConnectionState = ProspectiveConnectionState.Latent;
+
+    public delegate void StateChangeAlert(ProspectiveConnectionState newState);
+    public static event StateChangeAlert OnConnectingStateChange;
+
+
+    public static void ReportStateChange()
+    {
+        if (OnConnectingStateChange != null)
+        {
+            OnConnectingStateChange(currentConnectionState);
+        }
+    }
+
+}
