f5b417e94c6773ce7b6f9d88bd1f46dfac47ccbf
dcyoung <dcyoung@go.wustl.edu>
2016-05-07 22:40:41 -0500

    Enabled interactive selection of input and output ports + communication back to prospective connection event manager.

---
+++ FlowPath/Assets/InteractiveObjects/Connections/Scripts/ConnectionPort.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectionPort : MonoBehaviour {
+
+    public enum PortType
+    {
+        Input,
+        Output
+    }
+
+    public PortType portType;
+    private bool bIsConnected = false;
+
+    private Transform parent;
+    private SphereCollider trigger;
+    private MeshRenderer portMeshRenderer;
+
+    //Subscribe to Event: subscribes connection port to the mode manager to receive notice when the interaction mode changes
+    void OnEnable()
+    {
+        ModeManager.OnModeChange += EnableConnections;
+        ProspectiveConnectionManager.OnConnectingStateChange += LimitPortActivity;
+    }
+    //Unsubscribe from Event: when disabling this item, unsubscribe the connection port from the mode maanger
+    void OnDisable()
+    {
+        ModeManager.OnModeChange -= EnableConnections;
+        ProspectiveConnectionManager.OnConnectingStateChange -= LimitPortActivity;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        parent = transform.parent;
+        trigger = GetComponent<SphereCollider>();
+        portMeshRenderer = GetComponent<MeshRenderer>();
+        setActivePort(false);
+	}
+	
+    
+    //Change the active state of the port
+    private void setActivePort(bool bIsActive)
+    {
+        trigger.enabled = bIsActive;
+        portMeshRenderer.enabled = bIsActive;
+    }
+
+
+    //An event will report if the mode has been updated... change the presence of connection ports to match
+    void EnableConnections(InteractionMode newMode)
+    {
+        if(newMode == InteractionMode.ConnectionMode)
+        {
+            setActivePort(true);
+        }
+        else
+        {
+            setActivePort(false);
+        }
+    }
+
+    //An event will report if the prospective connection has changed states... change the presence of input or output connection ports to match
+    void LimitPortActivity(ProspectiveConnectionState newState)
+    {
+        switch (newState)
+        {
+            case ProspectiveConnectionState.Latent:
+                setActivePort(true);
+                break;
+            case ProspectiveConnectionState.Initiated:
+                //disable the port if its an input... because an input has already been specified for the prospective connection
+                setActivePort(portType == PortType.Output);
+                break;
+            case ProspectiveConnectionState.Completed:
+                setActivePort(false);
+                break;
+            default:
+                break;
+        }
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "indexTag")
+        {
+            if(portType == PortType.Input)
+            {
+                ProspectiveConnectionManager.SpecifyInputPort(transform.gameObject);
+            }
+            else if(portType == PortType.Output)
+            {
+                ProspectiveConnectionManager.SpecifyOutputPort(transform.gameObject);
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ FlowPath/Assets/InteractiveObjects/Connections/Scripts/ProspectiveConnectionManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum ProspectiveConnectionState
+{
+    Latent, //No nodes specified
+    Initiated,   //Start node has been specified
+    Completed    //Start AND End nodes HAVE been specified
+}
+
+public static class ProspectiveConnectionManager{
+
+    //The current connection to be formed. Will require a user to specify a start and end node.
+    public static ProspectiveConnectionState currentConnectionState = ProspectiveConnectionState.Latent;
+
+    public delegate void StateChangeAlert(ProspectiveConnectionState newState);
+    public static event StateChangeAlert OnConnectingStateChange;
+
+    private static GameObject inputPort;
+    private static GameObject outputPort;
+
+    public static void ReportStateChange()
+    {
+        if (OnConnectingStateChange != null)
+        {
+            OnConnectingStateChange(currentConnectionState);
+        }
+    }
+
+    public static void SpecifyInputPort(GameObject specifiedPort)
+    {
+        if(currentConnectionState == ProspectiveConnectionState.Latent)
+        {
+            MonoBehaviour.print("Registered an input port");
+            inputPort = specifiedPort;
+            currentConnectionState = ProspectiveConnectionState.Initiated;
+            ReportStateChange();
+        }
+        else
+        {
+            MonoBehaviour.print("You cannot specify an input port at this time.");
+        }
+    }
+
+    public static void SpecifyOutputPort(GameObject specifiedPort)
+    {
+        if (currentConnectionState == ProspectiveConnectionState.Initiated)
+        {
+            MonoBehaviour.print("Registered an output port");
+            outputPort = specifiedPort;
+            currentConnectionState = ProspectiveConnectionState.Completed;
+            ReportStateChange();
+        }
+        else
+        {
+            MonoBehaviour.print("You cannot specify an output port at this time.");
+        }
+    }
+
+
+}
