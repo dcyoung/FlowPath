428ba3db83101cb5b718e4250e710ad35a5331d2
dcyoung <dcyoung@go.wustl.edu>
2016-05-10 23:37:14 -0500

    Added holographic icons above the primitives

---
+++ FlowPath/Assets/InteractiveObjects/Prefabs/Primitives/Scripts/HologramRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HologramRotator : MonoBehaviour {
+
+    public float turnRate = 1.0f;
+    //private Quaternion startingOrientation; 
+
+	// Use this for initialization
+	void Start () {
+        //startingOrientation = transform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(0.0f, turnRate, 0.0f);
+	}
+}
